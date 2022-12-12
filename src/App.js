366d3edc3d9c05f3c4cb0692e2ebc694d7adbbd0
mohdshahbaz <mohdshahbaz027@gmail.com>
2022-12-13 00:58:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [word, setWord] = useState("");
+
+    const DisplayWord = () => {
+        return <span><b>Length: {word.length}</b></span>;
+    }
+
+    return (
+        <>
+            <Header />
+            <div className="container-fluid mt-5 d-flex flex-column align-items-center">
+                <input id="searchBox" type="text" className="form-control w-50" placeholder="Please enter a word..." onChange={(e) => setWord(e.target.value)} />
+                <div className="outputBox mt-5">
+                    <DisplayWord />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+    return (<h1 className="p-5 text-center text-bg-dark">Know the Word-Length</h1>)
+}
+
+export default Header;
